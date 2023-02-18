35315f826a112ef3edb16596d4af49168b248663
zzunho9 <jamlove4763@naver.com>
2023-02-18 20:27:53 +0900

    feat: Add craete and update method

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+mod entities;
+
 use dotenv_codegen::dotenv;
 use futures::executor::block_on;
-use sea_orm::{ConnectionTrait, Database, DbBackend, DbErr, Statement};
+use sea_orm::{*};
+use entities::{prelude::*, *};
 
-const DATABASE_URL:&str = dotenv!("DATABASE_URL");
+const DATABASE_URL:&str = " mysql://root:s2717244@localhost:3306";
 const DB_NAME:&str = dotenv!("DB_NAME");
 
 async fn run() -> Result<(), DbErr> {
-    let db = sea_orm::Database::connect(DATABASE_URL).await?;
+    let db = Database::connect(DATABASE_URL).await?;
 
     let db = &match db.get_database_backend() {
         DbBackend::MySql => {
@@ -33,6 +36,20 @@ async fn run() -> Result<(), DbErr> {
         }
         DbBackend::Sqlite => db,
     };
+
+    let happy_bakery = bakery::ActiveModel {
+        name: ActiveValue::Set("Happy Bakery".to_string()),
+        profit_margin: ActiveValue::Set(0.0),
+        ..Default::default()
+    };
+    let res = Bakery::insert(happy_bakery).exec(db).await?;
+
+    let sad_bakery = bakery::ActiveModel {
+        id: ActiveValue::Set(res.last_insert_id),
+        name: ActiveValue::Set("Sad Bakery".to_string()),
+        profit_margin: NotSet,
+    };
+    sad_bakery.update(db).await?;
 
     Ok(())
 }
